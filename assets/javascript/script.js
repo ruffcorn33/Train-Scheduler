81384956098c9e6bd3730f2d3bee1ba75f2ad6e2
Craig Christensen <ruffcorn@gmail.com>
2018-01-11 14:48:24 -0600

    Basic assignment finished.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,62 +1,83 @@
-// Initialize Firebase
+// initialize Firebase
 var config = {
-  apiKey: "AIzaSyB53ehUUkyjlWCBM0nvLiRmPAPt0vz61ZY",
-  authDomain: "test-project-a41bb.firebaseapp.com",
-  databaseURL: "https://test-project-a41bb.firebaseio.com",
-  projectId: "test-project-a41bb",
-  storageBucket: "test-project-a41bb.appspot.com",
-  messagingSenderId: "216203482760"
+  apiKey: "AIzaSyAq3mlCTUsep5WyHAhBaDVpjsBXqcFKKZQ",
+  authDomain: "trainschedules-3901b.firebaseapp.com",
+  databaseURL: "https://trainschedules-3901b.firebaseio.com",
+  projectId: "trainschedules-3901b",
+  storageBucket: "trainschedules-3901b.appspot.com",
+  messagingSenderId: "784696473453"
 };
 firebase.initializeApp(config);
 
-// Assign the reference to the database to a variable named 'database'
+// assign the reference to the database to a variable named 'database'
 // just for simplicity in referencing it
 var database = firebase.database();
 
-$("#add-user").on("click", function (event) {
-    event.preventDefault();
+$("#add-train").on("click", function(event) {
+  event.preventDefault();
+  // input values
+  var train = $("#train-input").val().trim();
+  var destination = $("#dest-input").val().trim();
+  var startTime = $("#time-input").val().trim();
+  var frequency = $("#freq-input").val().trim();
 
-    var name = $("#name-input").val().trim();
-    var role = $("#role-input").val().trim();
-    var date = $("#date-input").val().trim();
-    var rate = $("#rate-input").val().trim();
+  // add train record to Firebase db
+  database.ref().push({
+      train: train,
+      destination: destination,
+      startTime: startTime,
+      frequency: frequency,
+  });
 
-    console.log(name);
-    console.log(role);
-    console.log(date);
-    console.log(rate);
-
-
-
-    database.ref().push({
-        employee: name,
-        role: role,
-        date: date,
-        rate: rate,
-    });
-
-    $("#name-input").val("");
-    v$("#role-input").val("");
-    $("#date-input").val("");
-    $("#rate-input").val("");
-
-})
+  // clear input fields
+  $("#train-input").val("");
+  $("#dest-input").val("");
+  $("#time-input").val("");
+  $("#freq-input").val("");
+});
 
 database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val().employee);
-  console.log(childSnapshot.val().role);
-  console.log(childSnapshot.val().date);
-  console.log(childSnapshot.val().rate);
+  // move to variables
+  var trnName = childSnapshot.val().train;
+  var trnDest = childSnapshot.val().destination;
+  var trntime = childSnapshot.val().startTime;
+  var trnFreq = childSnapshot.val().frequency;
 
-  var empName = childSnapshot.val().employee;
-  var empRole = childSnapshot.val().role;
-  var empDate = childSnapshot.val().date;
-  var empRate = childSnapshot.val().rate;
+  // some momentjs time magic
+  var tFrequency = trnFreq;
+  // var firstTime = trntime;
 
-  var months = moment().diff(moment.unix(empDate, "X"), "months");
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(trntime, "hh:mm").subtract(1, "years");
 
-$("#tableHeader").append("<tr><td>"+empName+"</td><td>"+empRole+"</td><td>"+empDate+"</td><td>"+months+"</td><td>"+empRate+"</td><td></td></tr>");
+  // Current Time
+  var currentTime = moment();
 
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  nextTrain = moment(nextTrain).format("hh:mm")
+
+  // build table rows
+  $("tbody").append("<tr><td>"+trnName+"</td><td>"+trnDest+"</td><td>"+trnFreq+"</td><td>" + nextTrain + "</td><td id=\"nextTrn\">" + tMinutesTillTrain + "</td></tr>");
+
+  if (tMinutesTillTrain < 6) {
+    $("#nextTrn").attr("class", "imminent");
+  };
+
+  if (tMinutesTillTrain === 1) {
+    $("#nextTrn").attr("class", "arriving");
+  };
+
+  // handle Firebase errors
 }, function(errorObject) {
   console.log("Errors handled" + errorObject.code);
 });
